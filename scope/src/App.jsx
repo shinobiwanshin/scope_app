9e094f504838d64d2c7ba9eacca58e9c9f6acb11
Amitabha Nath <supersaiyan2k03@gmail.com>
2025-02-28 20:35:49 +0530

    error on the websocket part of the scope app

--- scope/src/App.jsx
+++ scope/src/App.jsx
@@ -3,9 +3,24 @@ import "./App.css";
 import LoginSignup from "./components/Login-Signup/LoginSignup";
 import { FrappeProvider } from "frappe-react-sdk";
 function App() {
+  const getSiteName = () => {
+    if (
+      window.frappe?.boot?.versions?.frappe &&
+      (window.frappe.boot.versions.frappe.startsWith("15") ||
+        window.frappe.boot.versions.frappe.startsWith("16"))
+    ) {
+      return window.frappe?.boot?.sitename ?? import.meta.env.VITE_SITE_NAME;
+    }
+    return import.meta.env.VITE_SITE_NAME;
+  };
   return (
     <>
-      <LoginSignup />
+      <FrappeProvider
+        // socketPort={import.meta.env.VITE_SOCKET_PORT}
+        siteName={getSiteName()}
+      >
+        <LoginSignup />
+      </FrappeProvider>
     </>
   );
 }
